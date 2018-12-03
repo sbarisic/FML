cd6e93f4f3c26c521236a67bb39ac59c856ab5d9
Saša Barišić <cartman300@net.hr>
2018-12-03 13:07:20 +0100

    Latest

--- FishMarkupLanguage/FML.cs
+++ FishMarkupLanguage/FML.cs
@@ -51,79 +51,176 @@ namespace FishMarkupLanguage {
 	}
 
 	public static class FML {
+		static int Line;
+		static int Col;
+
 		static TokenTuple[] SymbolTypes = new[] {
-			new TokenTuple("$", TokenType.PARAM_REF),
-			new TokenTuple("{", TokenType.BEGIN_BLOCK),
-			new TokenTuple("}", TokenType.END_BLOCK),
-			new TokenTuple("=", TokenType.ASSIGN),
-			new TokenTuple(";", TokenType.DELIMITER)
+			new TokenTuple("$", TokenType.DOLLAR),
+			new TokenTuple("#", TokenType.HASH),
+			new TokenTuple("{", TokenType.BRACKET_OPEN),
+			new TokenTuple("}", TokenType.BRACKET_CLOSE),
+			new TokenTuple("=", TokenType.EQUAL),
+			new TokenTuple(";", TokenType.SEMICOLON),
+			new TokenTuple(".", TokenType.DOT),
 		};
 
 		static string[] Keywords = new[] { "template", "none" };
 
-		static bool IsIDChar(char C) {
-			return char.IsLetterOrDigit(C) || C == '_' || C == '-';
+		static bool IsSymbol(char C) {
+			if (SymbolTypes.Select(T => T.Item1).Contains(C.ToString()))
+				return true;
+
+			return false;
 		}
 
-		static bool IsSymbol(char C) {
-			if (IsIDChar(C))
-				return false;
+		static bool IsNewLine(char C) {
+			if (C == '\n')
+				return true;
 
-			return C == '=' || C == '{' || C == '}' || C == ';' || char.IsSymbol(C) || char.IsPunctuation(C);
+			return false;
 		}
 
 		static bool IsWhiteSpace(char C) {
-			return char.IsWhiteSpace(C);
+			return IsNewLine(C) || char.IsWhiteSpace(C);
+		}
+
+		static bool IsIDChar(char C) {
+			if (IsWhiteSpace(C) || IsSymbol(C))
+				return false;
+
+			//return char.IsLetterOrDigit(C) || C == '_' || C == '-';
+			return true;
+		}
+
+		static void Fail(string Msg) {
+			throw new Exception(string.Format("{0}:{1}; {2}", Line + 1, Col, Msg));
+		}
+
+		static void Fail(string Fmt, params object[] Args) {
+			Fail(string.Format(Fmt, Args));
 		}
 
 		public static FMLDocument Parse(string FileName) {
 			string Source = File.ReadAllText(FileName).Replace("\r", "");
-			StringBuilder CmntCleaner = new StringBuilder();
-
-			bool InSingleLine = false;
-			bool InMultiLine = false;
-			for (int i = 0; i < Source.Length; i++) {
-				if (InSingleLine) {
-					while (Source[i] != '\n')
-						i++;
-
-					i--;
-					InSingleLine = false;
-					continue;
-				} else if (InMultiLine) {
-					while (!(Source[i] == '*' && Source[i + 1] == '/')) {
-						if (Source[i] == '\n')
-							CmntCleaner.Append('\n');
-						i++;
-					}
-
-					i++;
-					InMultiLine = false;
-					continue;
-				}
-
-				if (Source[i] == '/' && Source[i + 1] == '/') {
-					InSingleLine = true;
-					continue;
-				} else if (Source[i] == '/' && Source[i + 1] == '*') {
-					InMultiLine = true;
-					continue;
-				}
-
-				CmntCleaner.Append(Source[i]);
-			}
-
-			Source = CmntCleaner.ToString();
-
 
 			bool InQuote = false;
+			bool InSLComment = false;
+			bool InMLComment = false;
+
+			bool InDocument = false;
+			int DocEqLen = -1;
+
 			StringBuilder CurTok = new StringBuilder();
 			List<Token> Tokens = new List<Token>();
+			Action EmitIfNotEmpty = () => {
+				if (CurTok.Length > 0) {
+					EmitToken(CurTok.ToString(), Tokens);
+					CurTok.Clear();
+				}
+			};
+
+			Line = Col = 0;
 
 			for (int i = 0; i < Source.Length; i++) {
 				char C = Source[i];
 				char PC = i > 0 ? Source[i - 1] : (char)0;
 				char PPC = i > 1 ? Source[i - 2] : (char)0;
+				char NC = (i < Source.Length - 1) ? Source[i + 1] : (char)0;
+
+				Col++;
+				if (IsNewLine(C)) {
+					Col = 0;
+					Line++;
+				}
+
+				if (!InMLComment && !InDocument) {
+					if (C == '/' && NC == '/') {
+						EmitIfNotEmpty();
+						InSLComment = true;
+					}
+
+					if (InSLComment) {
+						if (IsNewLine(C)) {
+							InSLComment = false;
+							EmitIfNotEmpty();
+						}
+
+						continue;
+					}
+				}
+
+				if (!InSLComment && !InDocument) {
+					if (C == '/' && NC == '*') {
+						EmitIfNotEmpty();
+						InMLComment = true;
+					}
+
+					if (InMLComment) {
+						if (C == '*' && NC == '/') {
+							i++;
+							InMLComment = false;
+							EmitIfNotEmpty();
+						}
+
+						continue;
+					}
+				}
+
+				if (InDocument) {
+					if (C == ']') {
+						int BeginI = i;
+						bool ValidEnd = true;
+						int PrevDocEqLen = DocEqLen;
+
+						i++;
+
+						while (DocEqLen > 0) {
+							string END = Source.Substring(i);
+							char CCCC = Source[i + DocEqLen - 1];
+
+							if (Source[i + DocEqLen - 1] != '=') {
+								ValidEnd = false;
+								break;
+							}
+
+							DocEqLen--;
+						}
+
+						i += PrevDocEqLen;
+
+						if (ValidEnd && Source[i] != ']') 
+							ValidEnd = false;
+
+						if (ValidEnd) {
+							Tokens.Add(new Token(CurTok.ToString(), TokenType.DOCUMENT));
+							CurTok.Clear();
+							InDocument = false;
+							continue;
+						} else {
+							i = BeginI;
+							DocEqLen = PrevDocEqLen;
+						}
+					}
+
+					CurTok.Append(C);
+					continue;
+				}
+
+				if (C == '[') {
+					EmitIfNotEmpty();
+					InDocument = true;
+					DocEqLen = 0;
+					i++;
+
+					while (Source[i + DocEqLen] == '=')
+						DocEqLen++;
+
+					if (Source[i + DocEqLen] != '[')
+						Fail("Expected [, got {0}", Source[i + DocEqLen]);
+
+					i += DocEqLen;
+					continue;
+				}
 
 				if (C == '\"') {
 					if (!InQuote) {
@@ -163,9 +260,11 @@ namespace FishMarkupLanguage {
 			if (CurTok.Length > 0)
 				EmitToken(CurTok.ToString(), Tokens);
 
+			foreach (var T in Tokens)
+				Console.WriteLine(T);
+
 			FMLDocument Doc = new FMLDocument();
 			FMLTag CurTag = Doc.Root;
-			
 			return Doc;
 		}
 
@@ -175,14 +274,14 @@ namespace FishMarkupLanguage {
 			if (Tok.Length == 1 && IsSymbol(Tok[0])) {
 				T.Tok = SymbolTypes.Where(TT => TT.Item1 == Tok).FirstOrDefault()?.Item2 ?? TokenType.NONE;
 
-				// Special case to replace semicolon with an empty block
-				if (T.Tok == TokenType.DELIMITER) {
-					Tokens.Add(new Token() { Src = "{", Tok = TokenType.BEGIN_BLOCK });
+				/*// Special case to replace semicolon with an empty block
+				if (T.Tok == TokenType.SEMICOLON) {
+					Tokens.Add(new Token() { Src = "{", Tok = TokenType.BRACKET_OPEN });
 					T.Src = "}";
-					T.Tok = TokenType.END_BLOCK;
-				}
+					T.Tok = TokenType.BRACKET_CLOSE;
+				}*/
 			} else if (Tok.StartsWith("\"") && Tok.EndsWith("\"")) {
-				T.Tok = TokenType.QUOTE;
+				T.Tok = TokenType.STRING;
 			} else {
 				bool ValidID = true;
 
@@ -195,9 +294,9 @@ namespace FishMarkupLanguage {
 
 				if (ValidID) {
 					if (Keywords.Contains(Tok))
-						T.Tok = TokenType.KEYWORD;
+						T.Tok = TokenType.IDENTIFIER; // TODO: Keyword token?
 					else
-						T.Tok = TokenType.ID;
+						T.Tok = TokenType.IDENTIFIER;
 				}
 			}
 
@@ -213,6 +312,11 @@ namespace FishMarkupLanguage {
 		public string Src;
 		public TokenType Tok;
 
+		public Token(string Src, TokenType Tok) {
+			this.Src = Src;
+			this.Tok = Tok;
+		}
+
 		public override string ToString() {
 			string TokName = Tok.ToString();
 			int TokLen = 16;
@@ -226,13 +330,18 @@ namespace FishMarkupLanguage {
 
 	enum TokenType {
 		NONE,
-		ID,
-		PARAM_REF,
-		BEGIN_BLOCK,
-		END_BLOCK,
-		ASSIGN,
-		DELIMITER,
-		KEYWORD,
-		QUOTE
+
+		IDENTIFIER,
+		NUMBER,
+		STRING,
+		EQUAL,
+		BRACKET_OPEN,
+		BRACKET_CLOSE,
+		DOCUMENT,
+		SEMICOLON,
+		DOT,
+		DOLLAR,
+		HASH,
+		EOF
 	}
 }
