f55dc2254503c619f3069b8139076b7df893bc6c
Saša Barišić <sasa.barisic@serengetitech.com>
2021-11-15 14:13:07 +0100

    Scripting

---
+++ FishMarkupLanguage/Scripting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace FishMarkupLanguage {
+	public class Scripting {
+		Script<object> FMLScript;
+
+		public void Compile(string SrcCode, Type GlobalsType) {
+			FMLScript = CSharpScript.Create<object>(SrcCode, ScriptOptions.Default, GlobalsType);
+		}
+
+		public object Run(object Globals) {
+			ScriptState<object> State = FMLScript.RunAsync(Globals).Result;
+
+			if (State.Exception != null)
+				throw State.Exception;
+
+			return State.ReturnValue;
+		}
+	}
+}
